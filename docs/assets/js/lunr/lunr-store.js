77dd694a0842bfc2ffd8f146a29eb8d0e475cd54
mluerig <moritz.luerig@gmail.com>
2023-04-28 09:38:57 +0200

    gh pages

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "The 2nd Asellus symposium was held online",
+        "excerpt":"On November 26th 2021, the 2nd Asellus Symposium was held online. There were some 40+ registered participants from institutions in 14 countries. Below you can find the final program, links to all recorded talks on youtube (unlisted) and a sumary of the discussion notes. It was great to see the...","categories": [],
+        "tags": [],
+        "url": "/news/second-asellus-symposium",
+        "teaser": null
+      },{
+        "title": "Technical instructions: contributing to the wiki",
+        "excerpt":"Here is some information material for anyone interested in contributing to the website (www.asellus.org), or the wiki-page (www.asellus.org/wiki). For the time being, we will use a git-workflow and pull-requests, which can be done quite conveniently via GitHub. For that you need a GitHub account, and some very basic git knowledge....","categories": [],
+        "tags": ["manual","technical-doc"],
+        "url": "/news/technical-instructions-wiki",
+        "teaser": null
+      }]
